f3049f497266f28513a6dc3a09121fddae3c7a97
MukhiVivek <vivekmukhi07@gmail.com>
2025-04-19 05:52:07 +0530

    1.4.1

--- Frontend /src/landing_page/Signup.tsx
+++ Frontend /src/landing_page/Signup.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import { BACKEND_URL } from "../Config";
+// import axios from 'axios';
+// import { BACKEND_URL } from "../Config";
 
 const SignUp = () => {
   const [showPassword, setShowPassword] = useState(false);
