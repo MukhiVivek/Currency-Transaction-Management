aeed743ee80c188acd77b3a83c3f489053f5ead7
MukhiVivek <vivekmukhi07@gmail.com>
2025-04-24 05:41:52 +0530

    1.4.31

--- Frontend /src/Config.tsx
+++ Frontend /src/Config.tsx
@@ -1,5 +1,5 @@
-// export const BACKEND_URL = "https://currency-transaction-management.onrender.com"
+export const BACKEND_URL = "https://currency-transaction-management.onrender.com"
 
 // export const BACKEND_URL = "http://localhost:3000"
 
-export const BACKEND_URL = "http://3.110.159.55:3000"
+// export const BACKEND_URL = "http://3.110.159.55:3000"
